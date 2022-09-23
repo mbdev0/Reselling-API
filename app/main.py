3f5aec34083691c22a8e1a4026a979d6161b175a
mbdev0 <mustafababar7@gmail.com>
2022-09-23 20:59:25 +0100

    configured create endpoint properly

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-from models.object_models import *
+from models import object_models,crud
+from fastapi import Depends, FastAPI, HTTPException
+from configuration.dbconfig import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+from schemas import schemas
+
+object_models.Base.metadata.create_all(bind=engine)
+
+def interact_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app = FastAPI()
+
+@app.get("/")
+def response():
+    return({'message':'welcome'})
+
+@app.post("/user", response_model=schemas.UserCreation)
+def create_user(user: schemas.UserCreation, db: Session = Depends(interact_db)):
+    
+    return crud.create_user(db=db,user=user)
+
+
