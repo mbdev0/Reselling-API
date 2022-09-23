e23bb848da15b3d0916878bfcd0d7dcf41b11ee1
mbdev0 <mustafababar7@gmail.com>
2022-09-23 20:59:50 +0100

    removed crud functions within models

--- app/models/object_models.py
+++ app/models/object_models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, JSON
 from sqlalchemy.orm import relationship, Session
 
 from configuration.dbconfig import Base
-from schemas.schemas import *
+from schemas import schemas
 
 class User(Base):
     __tablename__ = 'users'
@@ -12,23 +12,7 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
-    storage = relationship("Storage", back_populates = 'users',uselist=False)
-
-    @classmethod
-    def create_user(db: Session, user: UserCreation):
-        print('hello')
-
-    """
-    Create a user 
-    Get by user id
-    Get by user email
-    Get a users storage ID
-    Update a users email
-    update a users password
-    delete a user by id
-    delete a user by email
-    """
-    
+    storage = relationship("Storage", backref = 'users',uselist=False)
 
 class Storage(Base):
     __tablename__ = 'user_storage'
@@ -37,18 +21,3 @@ class Storage(Base):
     userId = Column(Integer, ForeignKey("users.userid"))
     shoe_storage_space = Column(JSON)
     flips_storage_space = Column(JSON)
-    
-    user = relationship("User", back_populates = 'storage')
-
-    """
-    get shoe storage
-    get flips storage
-    add an item to shoe storage
-    add an item to flips storage
-    update an items -> any of its dict keys
-    delete an item from storage
-    delete an item from flips storage
-    clear the inventory -> flips and storage -> or either or
-    get total amount of retail for shoe or flips -> or both
-    get net profit (resell-retail) for shoe or flips -> or both
-    """
