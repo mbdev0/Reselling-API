5496f093fefdf7606023dd3d6b78e158a6bfac80
mbdev0 <mustafababar7@gmail.com>
2022-09-28 00:59:11 +0100

    added delete by id

--- app/main.py
+++ app/main.py
@@ -37,4 +37,8 @@ def get_user_by_id(user_id:int, db: Session = Depends(interact_db)):
 def update_user(user_id: int, user: schemas.User, db: Session = Depends(interact_db)):
     return crud.update_user(user_id=user_id, user=user, db=db)
 
+@app.delete('/user/{user_id}',response_model=dict)
+def delete_user_by_id(user_id:int, db: Session = Depends(interact_db)):
+    return crud.delete_user_by_id(user_id=user_id, db=db)
+
 
