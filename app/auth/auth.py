886fa92d2c5bca08b7a0c86d3e6ac5951583e7ea
mbdev0 <mustafababar7@gmail.com>
2022-10-29 20:43:06 +0100

    moved auth config into another file for secrecy

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,9 +1,23 @@
-from passlib.context import CryptContext
+from operations import crud
+from auth import authconfig
 
-passcontext = CryptContext(schemes=['bcrypt'],deprecated="auto")
+from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+
 
 def hash_pass(password:str) -> str:
-    return passcontext.hash(password)
+    return authconfig.passcontext.hash(password)
 
 def validate_pass(plain_pass:str,hashed_pass:str) -> bool:
-    return passcontext.verify(plain_pass, hashed_pass)
+    return authconfig.passcontext.verify(plain_pass, hashed_pass)
+
+def validate_user(formdata:OAuth2PasswordRequestForm,db:Session):
+    user = crud.get_user_by_username(username=formdata.username, db=db)
+    if not user:
+        raise HTTPException(status_code=401, detail='Incorrect Username or Password')
+    if not validate_pass(plain_pass=formdata.password,hashed_pass=user.password):
+        raise HTTPException(status_code=401, detail= 'Incorrect Email or Password')
+
+    return {"access_token": user.username, "token_type": "bearer"}
